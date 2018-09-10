ea56ef9c69b1594ba5970020e0e0dc5e76f0d20c
Jose F <bazoocaze@gmail.com>
2018-09-10 14:45:28 -0300

    First commit of project WikiDemoApp

---
+++ src/main/java/br/com/jasf/wikidemoapp/WikiDemoAppApplication.java
@@ -0,0 +1,12 @@
+package br.com.jasf.wikidemoapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WikiDemoAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WikiDemoAppApplication.class, args);
+	}
+}
